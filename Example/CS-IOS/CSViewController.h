fef22f4603010847defc82519f4408a443bf2a19
Rene Dohan <dohan.rene@gmail.com>
2018-03-08 12:27:30 -0600

    Initial commit

---
+++ Example/CS-IOS/CSViewController.h
@@ -0,0 +1,13 @@
+//
+//  CSViewController.h
+//  CS-IOS
+//
+//  Created by rene-dohan on 03/08/2018.
+//  Copyright (c) 2018 rene-dohan. All rights reserved.
+//
+
+@import UIKit;
+
+@interface CSViewController : UIViewController
+
+@end
---
+++ Example/CS-IOS/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  CS-IOS
+//
+//  Created by rene-dohan on 03/08/2018.
+//  Copyright (c) 2018 rene-dohan. All rights reserved.
+//
+
+@import UIKit;
+#import "CSAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([CSAppDelegate class]));
+    }
+}
