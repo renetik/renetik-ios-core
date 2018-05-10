ebde7803c5bcec772753eae636d51117fd9ae10c
Rene Dohan <dohan.rene@gmail.com>
2018-05-10 16:34:52 -0500

    0.4.0 in progress

--- CS-IOS/Classes/Core/Categories/Foundation/NSObject+CSExtension.h
+++ CS-IOS/Classes/Core/Categories/Foundation/NSObject+CSExtension.h
@@ -12,19 +12,19 @@
 
 - (instancetype)construct;
 
+- (instancetype)addNotificationObserver:(SEL)sel name:(NSString *)name;
+
+- (instancetype)addNotificationObserver:(SEL)sel name:(NSString *)name for:(id)object;
+
+- (instancetype)removeNotificationObserver;
+
 - (CSDoLaterProcess *)doLater:(void (^)(void))method;
 
 - (CSDoLaterProcess *)doLater:(double)seconds :(void (^)(void))method;
 
 - (CSWork *)schedule:(double)seconds :(void (^)(void))method;
 
-- (void)addNotificationCenterObserver:(SEL)sel name:(NSString *)name;
-
-- (void)addNotificationCenterObserver:(SEL)sel name:(NSString *)name for:(id)object;
-
 - (BOOL)isKindOfOneOfClass:(NSArray *)classes;
-
-- (void)removeNotificationObserver;
 
 - (NSString *)className;
 
--- CS-IOS/Classes/Core/Classes/Network/CSJSONData.m
+++ CS-IOS/Classes/Core/Classes/Network/CSJSONData.m
@@ -189,4 +189,5 @@
     }
     return array;
 }
+
 @end
