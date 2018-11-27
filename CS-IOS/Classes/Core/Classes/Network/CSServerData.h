8fc836d5fc9faa8a07636f0fa2c180020924a3cf
Rene Dohan <dohan.rene@gmail.com>
2018-11-26 19:11:31 -0600

    no message

--- CS-IOS/Classes/Core/Classes/Network/CSServerData.h
+++
@@ -1,9 +0,0 @@
-//
-// Created by Rene on 2018-11-21.
-//
-
-#import <Foundation/Foundation.h>
-
-
-@protocol CSServerData <NSObject>
-@end
---
+++ Example/Example/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Example for Renetik IOS
+//
+//  Created by Rene on 11/26/18.
+//  Copyright © 2018 rene-dohan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
