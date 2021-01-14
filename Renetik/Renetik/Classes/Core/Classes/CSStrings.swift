01591545c1db564a6df62083fc41ed356b89e49b
Rene Dohan <dohan.rene@gmail.com>
2021-01-14 02:07:22 -0600

    no message

--- Renetik/Renetik/Classes/Core/Classes/CSStrings.swift
+++ Renetik/Renetik/Classes/Core/Classes/CSStrings.swift
@@ -8,19 +8,19 @@ public typealias CSStrings = String
 
 public extension String {
     public static let cs_dialog_ok = localized("cs_dialog_ok")
-    public static let cs_dialog_yes = localized("renetik_dialog_yes")
-    public static let cs_dialog_no = localized("renetik_dialog_no")
-    public static let cs_dialog_cancel = localized("renetik_dialog_cancel")
+    public static let cs_dialog_yes = localized("cs_dialog_yes")
+    public static let cs_dialog_no = localized("cs_dialog_no")
+    public static let cs_dialog_cancel = localized("cs_dialog_cancel")
 
-    public static let requestRetry = localized("renetik_request_retry")
-    public static let requestCancel = localized("renetik_request_cancel")
-    public static let requestFailed = localized("renetik_request_failed_message")
-    public static let requestLoading = localized("renetik_request_loading")
+    public static let requestRetry = localized("cs_request_retry")
+    public static let requestCancel = localized("cs_request_cancel")
+    public static let requestFailed = localized("cs_request_failed_message")
+    public static let requestLoading = localized("cs_request_loading")
 
-    public static let tableLoadNextFailed = localized("renetik_table_load_next_failed_message")
+    public static let tableLoadNextFailed = localized("cs_table_load_next_failed_message")
 
-    public static let operationCancelled = localized("renetik_operation_canceled_message")
-    public static let operationFailed = localized("renetik_operation_failed_message")
+    public static let operationCancelled = localized("cs_operation_canceled_message")
+    public static let operationFailed = localized("cs_operation_failed_message")
 
-    public static let searchPlaceholder = localized("renetik_search_placeholder")
+    public static let searchPlaceholder = localized("cs_search_placeholder")
 }
