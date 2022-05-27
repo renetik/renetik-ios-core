47076bde7d9b7406bc4495e262c12f8fc44f908f
Rene Dohan <dohan.rene@gmail.com>
2022-05-27 01:28:23 -0500

    Update UIApplicationDelegate+CSExtension.swift

--- Renetik/Renetik/Classes/Core/Extensions/UIKit/UIApplicationDelegate+CSExtension.swift
+++ Renetik/Renetik/Classes/Core/Extensions/UIKit/UIApplicationDelegate+CSExtension.swift
@@ -5,11 +5,7 @@
 import UIKit
 
 public extension UIApplicationDelegate {
-
-    public static var instance: Self {
-        UIApplication.shared.delegate as! Self
-    }
-
+    public static var instance: Self { UIApplication.shared.delegate as! Self }
     public static var isDebug: Bool {
         #if DEBUG
         return true
