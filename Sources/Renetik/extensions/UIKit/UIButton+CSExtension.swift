cd999ff8cc1b4f3d533fdf58e6e5b7fd8296d905
Rene Dohan <dohan.rene@gmail.com>
2022-06-05 10:41:38 -0500

    ...

--- Sources/Renetik/extensions/UIKit/UIButton+CSExtension.swift
+++ Sources/Renetik/extensions/UIKit/UIButton+CSExtension.swift
@@ -36,19 +36,6 @@ public extension UIButton {
         return self
     }
 
-    func floatingDown(distance: CGFloat = 25) -> Self {
-        from(bottom: distance, right: distance)
-        if UIDevice.isTablet { resize(padding: 5) }
-        imageEdgeInsets = UIEdgeInsets(UIDevice.isTablet ? 20 : 15)
-        return self
-    }
-
-    func floatingUp(distance: CGFloat = 25) -> Self {
-        from(top: distance, right: distance)
-        if UIDevice.isTablet { resize(padding: 5) }
-        imageEdgeInsets = UIEdgeInsets(UIDevice.isTablet ? 20 : 15)
-        return self
-    }
 }
 
 extension UIButton: CSHasTextProtocol {
--- Sources/Renetik/extensions/UIKit/UIView+CSSafePosition.swift
+++
@@ -1,8 +0,0 @@
-//
-// Created by Rene Dohan on 2/14/20.
-//
-
-import UIKit
-
-public extension UIView {
-}
