95041a1834525f8c1ffe86649ae1d147974cce33
Rene Dohan <dohan.rene@gmail.com>
2026-05-24 16:13:40 +0800

    Refactor navigation hiding to use native UIKit properties.
    - Simplify `CSNavigationHidingController` by removing manual frame and visibility manipulation.
    - Utilize native `hidesBarsOnSwipe` and `hidesBarsWhenKeyboardAppears` properties.
    - Disable navigation hiding on tablet devices.
    - Update `MotorkariMainController` to enable or disable navigation hiding during the view lifecycle.

--- Renetik/Renetik/Classes/Core/Classes/Controller/CSNavigationHidingController.swift
+++ Renetik/Renetik/Classes/Core/Classes/Controller/CSNavigationHidingController.swift
@@ -1,37 +1,41 @@
 import RenetikObjc
 
 public class CSNavigationHidingController: CSMainController {
-    private var isNavigationBarHidden = false
-    private var shouldShow = false
-    private var isShowingRunning = false
-    private var shouldHide = false
-    private var isHidingRunning = false
     private let keyboardManager = CSKeyboardManager()
-    private var parentController: UIViewController!
 
     public func showIfNotKeyboard() {
-        if isNavigationBarHidden, !keyboardManager.isKeyboardVisible { requestNavigationBarShown() }
+        if !keyboardManager.isKeyboardVisible { requestNavigationBarShown() }
     }
 
     @discardableResult
     public func construct(by parent: UIViewController) -> Self {
         super.constructAsViewLess(in: parent)
-        parentController = parent
         keyboardManager.construct(self, onKeyboardChange)
+        enable()
         return self
     }
 
     private func onKeyboardChange(keyboardHeight: CGFloat) {
         if !isAppearing { return }
-        if keyboardHeight > 0, UIScreen.isLandscape {
+        enable()
+        if canHideNavigationBar, keyboardHeight > 0, UIScreen.isLandscape {
             requestNavigationBarHidden()
         } else {
             requestNavigationBarShown()
         }
     }
 
-    override public func onViewVisibilityChanged(_: Bool) {
-        isNavigationBarHidden = navigation.navigationBar.isHidden
+    override public func onViewWillAppear() {
+        super.onViewWillAppear()
+        enable()
+    }
+
+    override public func onViewVisibilityChanged(_ visible: Bool) {
+        if visible {
+            enable()
+        } else {
+            requestNavigationBarShown(animated: false)
+        }
     }
 
     override public func onViewDismissing() {
@@ -46,84 +50,32 @@ public class CSNavigationHidingController: CSMainController {
 
     override public func onViewDidTransition(to _: CGSize, _ _: UIViewControllerTransitionCoordinatorContext) {
         requestNavigationBarShown()
+        enable()
     }
 
-    private var lastDraggingContentOffset: CGFloat?
-
-    public func scrollViewWillBeginDragging(_ scrollView: UIScrollView) {
-        lastDraggingContentOffset = scrollView.contentOffset.y
+    private var canHideNavigationBar: Bool {
+        !UIDevice.isTablet
     }
 
-    public func scrollViewDidScroll(_ scrollView: UIScrollView) {
-        if scrollView.isAtTop {
-            requestNavigationBarShown()
-        } else if scrollView.isAtBottom {
-        } else {
-            if lastDraggingContentOffset.isNil { return }
-            if lastDraggingContentOffset! < scrollView.contentOffset.y - 400 {
-                requestNavigationBarHidden()
-            } else if lastDraggingContentOffset! > scrollView.contentOffset.y + 200 {
-                requestNavigationBarShown()
-            }
-        }
+    public func enable() {
+        navigation.hidesBarsOnSwipe = canHideNavigationBar
+        navigation.hidesBarsWhenKeyboardAppears = canHideNavigationBar && UIScreen.isLandscape
     }
 
-    public func scrollViewDidEndDragging(_: UIScrollView, willDecelerate decelerate: Bool) {
-        if !decelerate { lastDraggingContentOffset = nil }
-    }
+    public func scrollViewWillBeginDragging(_: UIScrollView) {}
 
-    public func scrollViewDidEndDecelerating(_: UIScrollView) {
-        lastDraggingContentOffset = nil
-    }
+    public func scrollViewDidScroll(_: UIScrollView) {}
 
-    public func requestNavigationBarHidden() {
-        if UIScreen.isUltraTall { return }
-        lastDraggingContentOffset = nil
-        if isNavigationBarHidden { return }
-        shouldHide = false
-        shouldShow = false
-        if isShowingRunning {
-            shouldHide = true
-            return
-        }
-        hideNavigationBar()
-    }
+    public func scrollViewDidEndDragging(_: UIScrollView, willDecelerate _: Bool) {}
 
-    private func hideNavigationBar(animated: Bool = true) {
-        isHidingRunning = true
-        isNavigationBarHidden = true
-        invoke(animated: animated, operation: {
-            navigation.navigationBar.bottom = UIApplication.statusBarBottom
-            self.parentController.view.height(fromTop: navigation.navigationBar.bottom)
-        }, completion: {
-            navigation.navigationBar.hide()
-            self.isHidingRunning = false
-            if self.shouldShow { self.requestNavigationBarShown() }
-        })
+    public func scrollViewDidEndDecelerating(_: UIScrollView) {}
+
+    public func requestNavigationBarHidden(animated: Bool = true) {
+        if !canHideNavigationBar { return }
+        navigation.setNavigationBarHidden(true, animated: animated)
     }
 
     public func requestNavigationBarShown(animated: Bool = true) {
-        lastDraggingContentOffset = nil
-        if !isNavigationBarHidden { return }
-        shouldShow = false
-        shouldHide = false
-        if isHidingRunning {
-            shouldShow = true
-            return
-        }
-        showNavigationBar(animated: animated)
-    }
-
-    private func showNavigationBar(animated: Bool = true) {
-        isShowingRunning = true
-        isNavigationBarHidden = false
-        navigation.navigationBar.show()
-        invoke(animated: animated, operation: {
-            navigation.navigationBar.top = UIApplication.statusBarBottom
-            self.parentController.view.height(fromTop: navigation.navigationBar.bottom)
-        }, completion: {
-            self.isShowingRunning = false
-            if self.shouldHide { self.requestNavigationBarHidden() }
-        })
+        navigation.setNavigationBarHidden(false, animated: animated)
     }
 }
